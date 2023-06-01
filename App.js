12ca18c742845079732a51c99f76a7c41c2e1df6
Shravya <lakshmi.shravya@airmeet.com>
2023-06-01 17:45:03 +0530

    Assignment-1

---
+++ App.js
@@ -0,0 +1,15 @@
+const Parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+const heading = React.createElement("h1", { id: "heading" }, "Hello world");
+
+const root = ReactDOM.createRoot(document.getElementById("roots"));
+
+root.render(Parent);
