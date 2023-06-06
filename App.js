4b6fda51610f7df760c1a386374ee648be085d14
Shravya <sannitishravya@gmail.com>
2023-06-06 21:23:23 +0530

    Episode/Assignment - 2

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
 const Parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am h1 tag"),
