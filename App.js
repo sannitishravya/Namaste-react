6a15f4c2d0443aeabb4fe483d43f69c1d2846192
Shravya <sannitishravya@gmail.com>
2023-06-09 10:29:25 +0530

    Assignment/Episode 3

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-const Parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
-const heading = React.createElement("h1", { id: "heading" }, "Hello world");
+const Component1 = ()=><div> <h1>Namaste React
 
+</h1></div>
+const Component2 = ()=> <div><Component1/><h1>Hello</h1></div>
 const root = ReactDOM.createRoot(document.getElementById("roots"));
 
-root.render(Parent);
+root.render(<Component2/>);
